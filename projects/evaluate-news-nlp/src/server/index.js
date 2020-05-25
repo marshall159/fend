056c9743c3dbf01c36f0a683eb64fe93b2204080
Aneel Marshall <aneelmarshall@outlook.com>
2020-05-25 17:41:07 +0100

    feat: add AYLIENTextAPI

--- projects/evaluate-news-nlp/src/server/index.js
+++ projects/evaluate-news-nlp/src/server/index.js
@@ -3,6 +3,11 @@ const express = require('express');
 const mockAPIResponse = require('./mockAPI.js');
 const dotenv = require('dotenv');
 dotenv.config();
+const AYLIENTextAPI = require('aylien_textapi');
+const textapi = new AYLIENTextAPI({
+    application_id: process.env.API_ID,
+    application_key: process.env.API_KEY
+});
 const PORT = process.env.PORT || 8081;
 
 const app = express();
@@ -22,5 +27,15 @@ app.listen(PORT, function () {
 });
 
 app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
+    textapi.sentiment({
+        'text': 'John is a very good football player!'
+    }, (error, response) => {
+        if (error === null) {
+            console.log(response);
+            res.send(response);
+        } else {
+            res.send(error);
+        }
+    });
+
 });
