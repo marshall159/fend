08819b81d8425148a14f0e5625e273c2faa76a18
Aneel Marshall <aneelmarshall@outlook.com>
2020-05-25 17:24:35 +0100

    feat: change port to env variable with fallback

--- projects/evaluate-news-nlp/src/server/index.js
+++ projects/evaluate-news-nlp/src/server/index.js
@@ -1,6 +1,7 @@
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
+const PORT = process.env.PORT || 8081;
 
 const app = express()
 
@@ -14,8 +15,8 @@ app.get('/', function (req, res) {
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(PORT, function () {
+    console.log(`Example app listening on port ${PORT}!`);
 })
 
 app.get('/test', function (req, res) {
