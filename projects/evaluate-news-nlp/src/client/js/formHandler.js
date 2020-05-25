6f37904dc27d09a928ebac416047a27fc849825c
Aneel Marshall <aneelmarshall@outlook.com>
2020-05-25 22:49:31 +0100

    feat: form to POST to /aylien route

--- projects/evaluate-news-nlp/src/client/js/formHandler.js
+++ projects/evaluate-news-nlp/src/client/js/formHandler.js
@@ -1,11 +1,38 @@
-function handleSubmit(event) {
+async function postData(url = '', data = {}) {
+    // Default options are marked with *
+    const response = await fetch(url, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'application/json'
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify(data)
+        // body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+
+    return response.json(); // parses JSON response into native JavaScript objects
+}
+
+async function handleSubmit(event) {
     event.preventDefault()
 
     // check what text was put into the form field
-    // let formText = document.getElementById('name').value
+    let formText = document.getElementById('name').value;
+
+    // const data = { formText: formText }
+
+    const response = await postData(
+        'http://localhost:8082/aylien', { formText }
+    );
     // checkForName(formText)
 
-    console.log("::: Form Submitted :::")
+    console.log("::: Form Submitted :::");
+    console.log('response :>> ', response);
     // fetch('http://localhost:8080/test')
     // .then(res => res.json())
     // .then(function(res) {
--- projects/evaluate-news-nlp/src/server/index.js
+++ projects/evaluate-news-nlp/src/server/index.js
@@ -9,16 +9,22 @@ const textapi = new AYLIENTextAPI({
     application_key: process.env.API_KEY
 });
 const PORT = process.env.PORT || 8082;
+const cors = require('cors');
+const bodyParser = require('body-parser')
 
 const app = express();
 
 app.use(express.static('dist'));
+app.use(cors());
+app.use(bodyParser.json());
 
-console.log(__dirname);
+// console.log(__dirname);
 
 app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    // res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile('dist/index.html');
+
 });
 
 // designates what port the app will listen to for incoming requests
@@ -26,16 +32,22 @@ app.listen(PORT, function () {
     console.log(`Example app listening on port ${PORT}!`);
 });
 
-app.get('/test', function (req, res) {
-    textapi.sentiment({
-        'text': 'John is a very good football player!'
-    }, (error, response) => {
-        if (error === null) {
-            console.log(response);
-            res.send(response);
-        } else {
-            res.send(error);
-        }
-    });
+app.post('/aylien', function (req, res) {
+    console.log('req :>> ', req.body);
+    // const { text } = req.body;
 
+    // textapi.sentiment({
+    //     text
+    // }, (error, response) => {
+    //     if (error === null) {
+    //         console.log(response);
+    //         res.send(response);
+    //     } else {
+    //         res.send(error);
+    //     }
+    // });
+
+    // console.log('text :>> ', text);
+
+    res.json('hello from aylien route');
 });
