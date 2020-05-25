bae447128ec90222ebd8ddfeb5d1055cfdd8b5a4
Aneel Marshall <aneelmarshall@outlook.com>
2020-05-25 17:30:34 +0100

    feat: add dotenv and env file for API keys

--- projects/evaluate-news-nlp/src/server/index.js
+++ projects/evaluate-news-nlp/src/server/index.js
@@ -1,24 +1,26 @@
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+var path = require('path');
+const express = require('express');
+const mockAPIResponse = require('./mockAPI.js');
+const dotenv = require('dotenv');
+dotenv.config();
 const PORT = process.env.PORT || 8081;
 
-const app = express()
+const app = express();
 
-app.use(express.static('dist'))
+app.use(express.static('dist'));
 
-console.log(__dirname)
+console.log(__dirname);
 
 app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
     res.sendFile(path.resolve('src/client/views/index.html'))
-})
+});
 
 // designates what port the app will listen to for incoming requests
 app.listen(PORT, function () {
     console.log(`Example app listening on port ${PORT}!`);
-})
+});
 
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
-})
+});
